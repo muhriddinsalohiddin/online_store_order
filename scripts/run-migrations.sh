0121f5ae6a93c830ab8fd42d4a999505f2adf744
Muhammadjon226 <valiyevmuhammadjon@gmail.com>
2022-01-09 16:12:49 +0500

    Migration down

--- scripts/run-migrations.sh
+++ scripts/run-migrations.sh
@@ -2,4 +2,5 @@
 export POSTGRES_USER=muhammad
 export POSTGRES_PASSWORD=12345
 export POSTGRES_DATABASE=book_order
+migrate -database "postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@localhost:5432/$POSTGRES_DATABASE?sslmode=disable" -path "./migrations"  down
 migrate -database "postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@localhost:5432/$POSTGRES_DATABASE?sslmode=disable" -path "./migrations"  up
